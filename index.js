a6e22092e5057f6faef073e3a84fdcef59b55b80
Abhishek <abhishekharshvardhan@gmail.com>
2021-02-03 12:19:21 +0530

    Add scrapper for fetching SMEs

---
+++ index.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const NodeXls = require('node-xls');
+const tool = new NodeXls();
+
+const URL = 'https://www.bizbaya.com';
+const getURI = (page) => URL + '/sme-india/delhi/new-delhi?page=' + page;
+
+const companies = [];
+
+async function fetchCompanies() {
+  for (let index = 1; index <= 21; index++) {
+    await fetchData(getURI(index)).then((res) => {
+      const html = res.data;
+      const $ = cheerio.load(html);
+      const links = $('.views-field-company-name >p >a');
+      links.each(function (i) {
+        let subLink = $(this).attr('href');
+        fetchData(URL + subLink).then((page) => {
+          const html = page.data;
+          const $ = cheerio.load(html);
+          companies.push({
+            company: $('.views-field-company-name > .field-content').text(),
+            directors: $('.views-field-directors > .field-content').text(),
+            mobile: $('.views-field-mobile > .field-content').text(),
+            telephone: $('.views-field-telephone > .field-content').text(),
+            email: $('.views-field-email > .field-content').text(),
+            address: $('.views-field-address > .field-content').text(),
+            pinCode: $('.views-field-pin > .field-content').text(),
+            products: $(
+              '.views-field-products-services > .field-content'
+            ).text(),
+          });
+
+          if (i === links.length - 1 && index === 21) {
+            const xls = tool.json2xls(companies);
+            fs.writeFileSync('companies.xlsx', xls, 'binary');
+          }
+        });
+      });
+    });
+  }
+}
+
+async function fetchData(url) {
+  let response = await axios(url).catch((err) => console.log(err));
+  if (response.status !== 200) {
+    console.log('Error occurred while fetching data');
+    return;
+  }
+  return response;
+}
+
+fetchCompanies();
